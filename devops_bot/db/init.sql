9b0d1dce5864350807fe6dc4149bb1d036e283fe
Serikenon <serzkonczov01@gmail.com>
2024-05-19 01:45:48 +0300

    Add project

--- devops_bot/db/init.sql
+++ devops_bot/db/init.sql
@@ -8,7 +8,7 @@ SELECT pg_create_physical_replication_slot('replication_slot');
 CREATE TABLE hba ( lines text );
 COPY hba FROM '/var/lib/postgresql/data/pg_hba.conf';
 INSERT INTO hba (lines) VALUES ('host all all 0.0.0.0/0 scram-sha-256');
-INSERT INTO hba (lines) VALUES ('host replication repl_user 0.0.0.0/0 scram-sha-256');
+INSERT INTO hba (lines) VALUES ('host replication ${DB_REPL_USER} 0.0.0.0/0 scram-sha-256');
 COPY hba TO '/var/lib/postgresql/data/pg_hba.conf';
 SELECT pg_reload_conf();
 
